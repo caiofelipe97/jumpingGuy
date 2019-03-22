e4825c57cbd7f0c6d6c6b391fb435594a069714d
Caio Felipe <caio.felipe@ext.tfgco.com>
2019-03-22 16:15:19 -0300

    first commit

--- Jumping Guy/Assets/Scripts/Player.cs
+++ Jumping Guy/Assets/Scripts/Player.cs
@@ -8,7 +8,12 @@ public class Player : MonoBehaviour
     public Vector2 speed = new Vector2(10, 10);
     private Vector2 movement = new Vector2(1, 1);
     private double jumpTime;
+    private double landTime;
     private bool onFloor = true;
+    private int combo = 1;
+    private int INTERVAL_BETWEEN_JUMPS = 60;
+    private int WALKING_TIME_LIMIT = 3000;
+
 
     Animator player;
 
@@ -19,39 +24,57 @@ public class Player : MonoBehaviour
 
     }
 
+
+
     // Update is called once per frame
     void Update()
-    {
-        double currentTime = (new TimeSpan(DateTime.Now.Ticks)).TotalMilliseconds;
+    {    
         float inputX = Input.GetAxis("Horizontal");
         float inputY = Input.GetAxis("Vertical");
 
+        double currentTime = (new TimeSpan(DateTime.Now.Ticks)).TotalMilliseconds;
+        double timeOnAir = currentTime - jumpTime;
+        double timeOnFloor = currentTime - landTime;
+        double jumpForce = WALKING_TIME_LIMIT / timeOnFloor;
+
         movement = new Vector2(speed.x * inputX, speed.y * inputY);
 
-        if (Input.GetKeyDown("space") && onFloor && currentTime - jumpTime >= 0)
+        if (Input.GetMouseButton(0) && onFloor && timeOnAir >= INTERVAL_BETWEEN_JUMPS)
         {
-            GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 10), ForceMode2D.Impulse);
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 5* combo), ForceMode2D.Impulse);
             player.SetTrigger("Jump");
             onFloor = false;
             jumpTime = currentTime;
 
+            if (timeOnFloor <= 50)
+            {
+                combo++;
+            }
+            else if(combo>1)
+            {
+                combo--;
+            }
         }
         
-        float minLeft = -4.6f;
-        float maxRight = 11.48f;
-        if (!onFloor)
-        {
-            float xUpdate = Input.acceleration.x;
-            if (xUpdate < minLeft)
-            {
-                xUpdate = minLeft;
-            }
-            if (xUpdate > maxRight)
-            {
-                xUpdate = maxRight;
-            }
-            transform.Translate(xUpdate, 0, 0);
-        }
+        //float minLeft = -4.6f;
+        //float maxRight = 11.48f;
+        ////if (!onFloor)
+        ////{
+        ////    float xUpdate = Input.acceleration.x;
+        ////    if (xUpdate < minLeft)
+        ////    {
+        ////        xUpdate = minLeft;
+        ////    }
+        ////    if (xUpdate > maxRight)
+        ////    {
+        ////        xUpdate = maxRight;
+        ////    }
+        ////    transform.Translate(xUpdate, 0, 0);
+        ////}
+        ////else
+        ////{
+        ////    transform.Translate(0, 0, 0);
+        ////}
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -59,6 +82,7 @@ public class Player : MonoBehaviour
         if(collision.gameObject.tag=="Floor")
         {
             onFloor = true;
+            landTime = (new TimeSpan(DateTime.Now.Ticks)).TotalMilliseconds;
         }
 
     }
